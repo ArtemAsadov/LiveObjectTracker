c7471bcb366733e0f427da70fe7fc79542040cf0
Artem Asadov <kekc33rus@gmail.com>
2026-06-02 12:09:10 +0300

    Добавил TCP listner

--- LiveObjectTracker.Service/Program.cs
+++ LiveObjectTracker.Service/Program.cs
@@ -1,7 +1,9 @@
 ﻿using LiveObjectTracker.Service.Models;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Channels;
 
-Console.WriteLine("=== Start ===");
+Console.WriteLine("=== Live Object Tracker ===");
 
 // TODO 1.2: TCP-сервер + BoundedChannel (10_000, Wait mode)
 // TODO 1.2.1: Добавляем BoundedChannel и базовую структуру
@@ -19,10 +21,49 @@ var channel = Channel.CreateBounded<CoordinateEvent>(
         SingleWriter = false,
     });
 
-Console.WriteLine("=== Live Object Tracker ===");
+
 Console.WriteLine($"Config: port={TcpPort}, capacity={ChannelCapacity}, workers={WorkersCount}");
-Console.WriteLine($"Channel created: {channel.GetType().Name}");
-Console.WriteLine($"Channel is bounded: {channel.Reader.CanCount}");
+
+// TCP - сервер
+_ = Task.Run(async () =>
+{
+    var listener = new TcpListener(IPAddress.Loopback, TcpPort);
+    listener.Start();
+    Console.WriteLine($"[TCP] Listening on port {TcpPort}...");
+
+    while (true)
+    {
+        var client = await listener.AcceptTcpClientAsync();
+        Console.WriteLine($"[TCP] Client connected from {client.Client.RemoteEndPoint}");
+
+        _ = Task.Run(() => HandleClientAsync(client, channel.Writer));
+    }
+});
+
+Console.WriteLine("[TCP] Server is running");
+
+static async Task HandleClientAsync(TcpClient client, ChannelWriter<CoordinateEvent> writer)
+{
+    using var stream = client.GetStream(); //Dispose при socket.Closed()
+    var buffer = new byte[1024];
+
+    try
+    {
+        while (true)
+        {
+            var bytesRead = await stream.ReadAsync(buffer);
+            if (bytesRead == 0) break; // клиент отключился
+
+            //TODO: Распарсить JSON и отправить в channel
+            Console.WriteLine($"[TCP] Received {bytesRead} bytes");
+        }
+    }
+    finally
+    {
+        client.Close();
+        Console.WriteLine($"[TCP] Client disconnected");
+    }
+}
 
 // TODO 1.3: Worker pool (TaskCreationOptions.LongRunning)
 // TODO 1.4: Generator-client (~10k RPS)
